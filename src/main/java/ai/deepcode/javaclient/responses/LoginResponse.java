a445aa3d7ebaaf412cf2e843a4d7d0c0294f27d9
Artsiom Chapialiou <achapialiou@gmail.com>
2020-03-17 17:09:59 +0300

    make LoginResponse immutable.

--- src/main/java/ai/deepcode/javaclient/responses/LoginResponse.java
+++ src/main/java/ai/deepcode/javaclient/responses/LoginResponse.java
@@ -1,11 +1,19 @@
 package ai.deepcode.javaclient.responses;
 
 public class LoginResponse {
-  public final String sessionToken;
-  public final String loginURL;
+  private final String sessionToken;
+  private final String loginURL;
 
   private LoginResponse(String sessionToken, String loginURL) {
     this.sessionToken = sessionToken;
     this.loginURL = loginURL;
+  }
+
+  public String getSessionToken() {
+    return sessionToken;
+  }
+
+  public String getLoginURL() {
+    return loginURL;
   }
 }
